fa8b28a33a0635a1fd4cbf4f469b921cb50125ec
Sam Garson <samtgarson@gmail.com>
2017-02-11 16:02:40 +0000

    new micro-nuxt app with xo

---
+++ api/users.js
@@ -0,0 +1,8 @@
+module.exports = new Promise(resolve => {
+  resolve([
+      { name: 'Alexandre' },
+      { name: 'Sébastien' },
+      { name: 'Sebastian' }
+  ]
+  )
+})
---
+++ nuxt.index.js
@@ -0,0 +1,19 @@
+const Nuxt = require('nuxt')
+let config = require('./nuxt.config.js')
+
+// Import and Set Nuxt.js options
+config.dev = !(process.env.NODE_ENV === 'production')
+
+// Init Nuxt.js
+const nuxt = new Nuxt(config)
+
+// Build only in dev mode
+if (config.dev) {
+  nuxt.build()
+  .catch(err => {
+    console.error(err) // eslint-disable-line no-console
+    process.exit(1) // eslint-disable-line unicorn/no-process-exit
+  })
+}
+
+module.exports = nuxt
