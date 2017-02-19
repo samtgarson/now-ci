bf71a745c66a88c4c6b4efc49086b58313831456
Sam Garson <samtgarson@gmail.com>
2017-02-19 20:02:39 +0000

    Use nav menu on mobile

--- client/components/nav.vue
+++ client/components/nav.vue
@@ -4,6 +4,9 @@ import NavItem from './nav-item'
 
 export default {
   computed: mapState(['user']),
+  data () {
+    return { open: false }
+  },
   components: { NavItem }
 }
 </script>
@@ -15,15 +18,21 @@ export default {
       a.nav-item(href="https://github.com/samtgarson/now-ci")
         span(class="icon is-small")
           i(class="fa fa-github")
-    .nav-right(v-if="user.loggedIn")
+    .nav-toggle(@click="open = !open")
+      span
+      span
+      span
+    .nav-right.nav-menu(v-if="user.loggedIn", :class="{ 'is-active': open }")
       nav-item.nav-item(path="/") Builds
       nav-item.nav-item(path="/settings") Settings
       .nav-item
           a.button(href="/logout") Logout
+
 </template>
 <style lang="sass" scoped>
 @import '~bulma/sass/utilities/variables'
 
 #avatar
   border-radius: 24px
+
 </style>
